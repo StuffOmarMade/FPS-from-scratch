241db4313377b04299d27c1018a28b6adecabb67
stuffomarmade <work.omarkhaled@gmail.com>
2025-05-13 06:26:00 +0300

    Refactored whole code.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pyTGA
 import math
 import numpy
-from stb import image as im
 
 import pygame
 from pygame.locals import *
@@ -9,209 +7,208 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-width = 512
-height = 512
-block_size = 32
+class World:
+    
+    def __init__(self, width, height, tile_size, world_map):
+        self.width = width
+        self.height = height
+        self.tile_size = tile_size
+        self.world_map = world_map
 
-player_x = 40
-player_y = 64
-player_size = 8
+class Player:
 
-player_a = 40
-fov = 60
-fov_rad = math.radians(fov)
+    def __init__(self, height, x, y, fov, angle, movement_speed, turning_speed):
+        self.height = height
+        self.x = x
+        self.y = y
+        self.fov = fov
+        self.angle = angle
+        self.movement_speed = movement_speed
+        self.turning_speed = turning_speed
+        self.halfFOV = fov / 2
 
-distance_to_projection_plane = (width / 2) / math.tan(fov_rad / 2)
-
-world_map = [[0 for i in range(int(height / block_size))] for j in range(int(width / block_size))]
-
-block_list = [
-    [1, 6],
-    [1, 7],
-    [1, 8],
-    [1, 9],
-    [1, 10],
-    [1, 11],
-
-    [2, 5],
-    [3, 5],
-    [4, 5],
-
-    [5, 3],
-    [5, 4],
-    [5, 5],
-    [5, 6],
-    [5, 7],
-
-    [6, 3],
-    [7, 3],
-    [8, 3],
-
-    [6, 7],
-    [7, 7],
-    [8, 7],
-    [9, 7],
-    [10, 7],
-    [11, 7],
-    [12, 7],
-    [13, 7],
-    [14, 7],
-
-    [14, 1],
-    [14, 2],
-    [14, 3],
-    [14, 4],
-    [14, 5],
-    [14, 6],
-
-    [3, 8],
-    [3, 9],
-    [3, 10],
-    [3, 11],
-    [3, 12],
-    [3, 13],
-    [3, 14],
-    [3, 15],
-
-    [6, 8],
-    [6, 9],
-    [6, 10],
-    [6, 11],
-
-    [8, 10],
-    [8, 11],
-    [8, 12],
-    [8, 13],
-    [8, 14],
-    [8, 15]
+world_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
-for block in block_list:
-        world_map[block[0]][block[1]] = 1
+world = World(512, 512, 64, world_map)
+player = Player(32, 100, 100, 60, 90, 10, 10)
 
-x = im.load('BIGSQUARES.png')
+angle_between_subsequent_columns = player.fov / world.width
+distance_to_projection_plane = (world.width / 2) / math.tan(math.radians(player.halfFOV))
 
-def init_image(width, height, background_color):
-    data = [[background_color] * width ] * height
-    image = pyTGA.Image(data)
+def raycast(angle):
+    horizontal_hit = False
+    vertical_hit = False
 
-    return image
+    # horizontal case
+    if not ((angle > 0) and (angle < math.pi)): # angle is facing up
+        intersection_y = ((player.y // world.tile_size) * world.tile_size) - 0.01
+        ya = -world.tile_size
+    else:
+        intersection_y = ((player.y // world.tile_size) * world.tile_size) + world.tile_size
+        ya = world.tile_size
 
-# frame_buffer = init_image(width, height, (0, 0, 0, 255))
-# rendered_img = init_image(width, height, (255, 255, 255, 255))
+    intersection_x = ((intersection_y - player.y) / math.tan(angle)) + player.x
+    xa = ya / math.tan(angle)
+    
+    while (not horizontal_hit) and (intersection_x <= world.width and intersection_x >= 0 and intersection_y <= world.height and intersection_y >= 0):
+        horizontal_map_x = int(intersection_x // world.tile_size)
+        horizontal_map_y = int(intersection_y // world.tile_size)
+    
+        if world_map[horizontal_map_y][horizontal_map_x]:
+            horizontal_hit = True
+            horizontalHitPos = [intersection_x, intersection_y]
+        else:
+            intersection_x += xa
+            intersection_y += ya
 
-def set_pixel(x, y, color):
-    glBegin(GL_POINTS)
-    glColor(color[0] / 255, color[1] / 255, color[2] / 255, color[3] / 255)
-    glVertex2f(x, y)
+    # vertical case
+    if (angle < 0.5 * math.pi) or (angle > 1.5 * math.pi):
+        intersection_x = ((player.x // world.tile_size) * world.tile_size) + world.tile_size
+        xa = world.tile_size
+    else:
+        intersection_x = ((player.x // world.tile_size) * world.tile_size) - 0.01
+        xa = -world.tile_size
+
+    intersection_y = ((intersection_x - player.x) * math.tan(angle)) + player.y
+    ya = xa * math.tan(angle)
+
+    while (not vertical_hit) and (intersection_x <= world.width and intersection_x >= 0 and intersection_y <= world.height and intersection_y >= 0):
+        vertical_map_x = int(intersection_x // world.tile_size)
+        vertical_map_y = int(intersection_y // world.tile_size)
+    
+        if world_map[vertical_map_y][vertical_map_x]:
+            vertical_hit = True
+            verticalHitPos = [intersection_x, intersection_y]
+        else:
+            intersection_x += xa
+            intersection_y += ya
+    
+    # distance calc
+    if horizontal_hit:
+        horizontalDistance = math.sqrt(math.pow(horizontalHitPos[0] - player.x, 2) + math.pow(horizontalHitPos[1] - player.y, 2))
+    
+    if vertical_hit:
+        verticalDistance = math.sqrt(math.pow(verticalHitPos[0] - player.x, 2) + math.pow(verticalHitPos[1] - player.y, 2))
+    
+    if horizontal_hit and vertical_hit:
+        if(horizontalDistance < verticalDistance):
+            draw_ray(horizontalHitPos[0], horizontalHitPos[1])
+            return horizontalDistance
+        else:
+            draw_ray(verticalHitPos[0], verticalHitPos[1])
+            return verticalDistance
+            
+    elif horizontal_hit:
+        draw_ray(horizontalHitPos[0], horizontalHitPos[1])
+        return horizontalDistance
+    elif vertical_hit:
+        draw_ray(verticalHitPos[0], verticalHitPos[1])
+        return verticalDistance
+
+def render(col, distance_to_slice):
+    projected_slice_height = (world.tile_size / distance_to_slice) * distance_to_projection_plane
+    glBegin(GL_LINES)
+    # glColor(0, 0, 0, 1)
+    # glVertex2d(col + world.width, 0)
+    # glVertex2d(col + world.width, (world.height / 2) - (projected_slice_height / 2))
+
+    glColor(149/255, 6/255, 6/255, 1)
+    glVertex2d(col + world.width, (world.height / 2) - (projected_slice_height / 2))
+    glVertex2d(col + world.width, (world.height / 2) + (projected_slice_height / 2))
+    
+    # glColor(0.5, 0.5, 0.5, 1)
+    # glVertex2d(col + world.width, (world.height / 2) + (projected_slice_height / 2))
+    # glVertex2d(col + world.width, world.height)
     glEnd()
 
-def draw_block(x, y, size, color):
-    for i in range(size):
-        for j in range(size):
-            set_pixel(j + x, i + y, color)
+def display_map(world_map):
+    glBegin(GL_QUADS)
+    for i in range(0, len(world_map) * world.tile_size, world.tile_size):
+        for j in range(0, len(world_map[i // world.tile_size]) * world.tile_size, world.tile_size):
+            if world_map[i // world.tile_size][j // world.tile_size]:
+                glColor(1, 1, 1, 1)
+            else:
+                glColor(0, 0, 0, 1)
+            glVertex2d(j, i)
+            glVertex2d(j, i + world.tile_size)
+            glVertex2d(j + world.tile_size, i  + world.tile_size)
+            glVertex2d(j + world.tile_size, i)
+    glEnd()
 
-#def draw_blocks(img):
-#    for block in block_list:
-#       for i in range(block_size):
-#            for j in range(block_size):
-#                img.set_pixel(j + (block_size * block[0]), i + (block_size * block[1]), (255, 255, 255, 0))
+def draw_player(player):
+    glPointSize(16)
+    glColor(1, 0, 0, 1)
+    glBegin(GL_POINTS)
+    glVertex2d(player.x, player.y)
+    glEnd()
 
-def single_raycast(col, angle):
-        global player_y
+def draw_ray(x, y):
+    glBegin(GL_LINES)
+    glColor(0.3, 0.3, 0.3, 1)
+    glVertex2d(player.x, player.y)
+    glVertex2d(x, y)
+    glEnd()
 
-        angle_in_rad = math.radians(angle)
+def handlePlayerMovement():
+    keys = pygame.key.get_pressed()
 
-        adj = math.cos(angle_in_rad)
-        opp = math.sin(angle_in_rad)
+    if keys[K_RIGHT]:
+        player.x += (-math.sin(math.radians(player.angle)) *  player.movement_speed)
+        player.y += (math.cos(math.radians(player.angle)) * player.movement_speed)
+    if keys[K_LEFT]:
+        player.x += (math.sin(math.radians(player.angle)) *  player.movement_speed)
+        player.y -= (math.cos(math.radians(player.angle)) * player.movement_speed)
+    if keys[K_UP]:
+        player.x += (math.cos(math.radians(player.angle)) *  player.movement_speed)
+        player.y += (math.sin(math.radians(player.angle)) * player.movement_speed)
+    if keys[K_DOWN]:
+        player.x -= (math.cos(math.radians(player.angle)) *  player.movement_speed)
+        player.y -= (math.sin(math.radians(player.angle)) * player.movement_speed)
 
-        for h in range(0, height, 1):
-            pixel_x = (player_x + (player_size / 2)) + (adj * h)
-            pixel_y = (player_y + (player_size / 2)) + (opp * h)
-
-            if((pixel_x < 0 or pixel_y < 0) or (pixel_x > width or pixel_y > height)):
-                return
-
-            map_x = int(pixel_x / block_size)
-            map_y = int(pixel_y / block_size)
-
-            if world_map[map_y][map_x]: # inverted coordinates
-                if (((int(pixel_x) % block_size) == 0) or ((int(pixel_x) % block_size) == 31)):
-                    img_col = int(pixel_y) - (block[0] * block_size) # vertically
-                elif (((int(pixel_y) % block_size) == 0) or ((int(pixel_y) % block_size) == 31)):
-                    img_col = int(pixel_x) - (block[1] * block_size) # horizontally
-                
-                render(col, pixel_x, pixel_y, img_col)
-
-                return
-    
-
-        #    draw_block(int(pixel_x), int(pixel_y), 1, (255, 255, 255, 255))
-
-def raycast(player_angle, fov):
-    col = 0
-    for angle in numpy.arange(player_angle - (fov / 2), (fov / 2) + player_angle, (fov / 512)):
-        single_raycast(col, angle)
-        col += 1
-
-def render(col, pixel_x, pixel_y, img_col = 0):
-    global player_y
-
-    distance_to_the_slice = math.sqrt(math.pow((player_x - pixel_x), 2) + math.pow((player_y - pixel_y), 2))
-    projected_slice_height = (block_size / distance_to_the_slice) * distance_to_projection_plane
-    start_pos = (height / 2) - (projected_slice_height / 2)
-
-    for i in range(int(start_pos)):
-        draw_block(col, i, 1, (63, 23, 23, 255))
-
-    for i in range(0, int(projected_slice_height)):
-        # to be fixed
-        #resized_col = numpy.resize(x[:, img_col], (int(projected_slice_height), 4))
-
-        #draw_block(col, int(start_pos) + i, 1, resized_col[i])
-        draw_block(col, int(start_pos) + i, 1, (0, 255, 0, 255))
-
-    for i in range(int(start_pos) + int(projected_slice_height), height):
-        draw_block(col, i, 1, (149, 6, 6, 255))
-
-
+    if keys[K_z]:
+        player.angle -= player.turning_speed
+    if keys[K_x]:
+        player.angle += player.turning_speed
 
 def main():
-    #frame_buffer.set_first_pixel_destination('tl')
-    #rendered_img.set_first_pixel_destination('tl')
-
-    #draw_blocks(frame_buffer)
-    #draw_block(player_x, player_y, player_size, (255, 0, 0, 255), frame_buffer)
-
-    # frame_buffer.save("frame_buffer")
-    
-
-    global player_y
-    
     pygame.init()
-    display = (width, height)
+    display = (1024, 512)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluOrtho2D(0, width, height, 0) 
+    gluOrtho2D(0, 1024, 512, 0)  # left, right, bottom, up
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    player_y += 10
-                
-                if event.key == pygame.K_DOWN:
-                    player_y -= 10
-        
-        glClear(GL_COLOR_BUFFER_BIT)
 
-        raycast(player_a, fov) 
+        handlePlayerMovement()
+            
+        glClear(GL_COLOR_BUFFER_BIT)
+        
+        display_map(world_map)
+        draw_player(player)
+
+        col = 0
+        for angle in numpy.arange(player.angle - (player.halfFOV), player.angle + (player.halfFOV), angle_between_subsequent_columns):
+            angle = math.radians(angle)
+            angle = angle % (2 * math.pi)
+            if angle <= 0:
+                angle = (2 * math.pi) + angle
+            distance_to_slice = raycast(angle)
+            render(col, distance_to_slice * math.cos(math.radians(player.angle) - angle))
+            col += 1
         
         pygame.display.flip()
         pygame.time.wait(10)
-    
 
 main()
